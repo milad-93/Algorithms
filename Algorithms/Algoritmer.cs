d6226c78b252fb3892dedc6022148efbbb6db43c
Milad <mima0019@student.hv.se>
2020-10-09 03:00:55 +0200

    added all algorithms

--- Algorithms/Algoritmer.cs
+++ Algorithms/Algoritmer.cs
@@ -29,7 +29,7 @@ namespace Algorithms
 
             for (int i = 0; i < array.Length; i++)
             {
-                array[i] = rnd.Next(0, 10 * array.Length);
+                array[i] = rnd.Next(0, 10 * array.Length); // 10 times
             }
 
         }
@@ -87,16 +87,80 @@ namespace Algorithms
 
         }
 
-        public static void MergeSort(int[] array)
+        #region MergeSort
+        public static void Sort(int[] array, int right, int left)
         {
-            Console.WriteLine("hello"); 
+            int mid;
+            if (right > left)     //low >= high is the base case, i.e. there is 0 or 1 item   
+            {
+                mid = (right + left) / 2;
+                Sort(array, left, mid);          // divide numbers into two halves   and 
+                Sort(array, (mid + 1), right);   //recursively sort them   
+                Merge(array, left, (mid + 1), right);    // conquer ,merge two sorts halves
+            }
         }
 
+        public static void Merge(int[] array, int left, int mid, int right)  //a recursive function to merge numbers[left,mid] and numbers[mid+1,right] into numbers[left,right]
+        {
+            int[] temp = new int[25];    // temp is a temporary array to store result
+            int i, left_end, num_elements, tmp_pos;
+            left_end = (mid - 1);
+            tmp_pos = left;
+            num_elements = (right - left + 1);
+            while ((left <= left_end) && (mid <= right)) //merging when tow halves have unsorted items
+            {
+                if (array[left] <= array[mid])
+                    temp[tmp_pos++] = array[left++];
+                else
+                    temp[tmp_pos++] = array[mid++];
+            }
+            while (left <= left_end) temp[tmp_pos++] = array[left++]; //remaining items are copied into temp
+            while (mid <= right) temp[tmp_pos++] = array[mid++];
+            for (i = 0; i < num_elements; i++)
+            {
+                array[right] = temp[right]; right--; 
+
+            }
+            //Merged result are copied back //into array[]
+        }
+
+        #endregion MergeSort
 
 
+        public static void QuickSort(int[] array, int left, int right) // uses the function Partition
+        {
+            if (left > right || left < 0 || right < 0) return;
 
+            int index = Partition(array, left, right);
 
-        #endregion
+            if (index != -1)
+            {
+                QuickSort(array, left, index - 1);
+                QuickSort(array, index + 1, right);
+            }
+        }
+
+        private static int Partition(int[] array, int left, int right)
+        {
+            if (left > right) return -1;
+
+            int end = left;
+
+            int pivot = array[right];    // choose last one to pivot, easy to code
+            for (int i = left; i < right; i++)
+            {
+                if (array[i] < pivot)
+                {
+                    Swap(array, i, end);
+                    end++;
+                }
+            }
+
+            Swap(array, end, right);
+
+            return end;
+        }
+        #endregion Algorithms
 
 
 
